56e867a563b1473d9bd5885ccbfdccf1082d087b
Paulagarcia17 <130710710+Paulagarcia17@users.noreply.github.com>
2023-05-23 16:16:28 -0300

    Clase 10

---
+++ js/javascript.js
@@ -0,0 +1,15 @@
+let nombre= "Paula" ;
+let apellido= "Garcia" ;
+
+console.log (nombre + " " +apellido)
+
+let numero1= 150 ;
+let numero2= 100 ;
+
+let suma=(numero1+numero2);
+
+console.log (suma)
+
+let sumatotal= (suma+50)
+
+console.log (sumatotal)
